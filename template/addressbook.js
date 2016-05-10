f7ba0775e92be1e75066ff764f0b67c4eaf6fa4c
Akira Yoshida <akiracraftwork@gmail.com>
2016-05-10 11:03:09 +0900

    ZipCodeをベースとして作成

---
+++ template/addressbook.js
@@ -0,0 +1,69 @@
+angular.module('myApp', [] )
+  .controller( 'zipCode2Address',
+  [ '$scope', '$http', function($scope, $http){
+    　 $scope.zipcode = "";
+      $scope.onClickGetAddress = function() {
+        // 全角だったら半角に変換
+        zipcode = toHalfWidth($scope.zipcode);
+        // 郵便番号の正規表現
+        var reg = new RegExp(/^\d{3}-?\d{4}$/);
+        // 入力されていない時は変換できない
+        if ( zipcode.length == 0 ) {
+          $scope.result = "郵便番号が入力されていません";
+          return;
+        }
+        // nnn-mmmmまたはnnnmmmmのパターンだけを郵便番号とする
+        else if ( !reg.test(zipcode) ) {
+          $scope.result = "郵便番号が不正です";
+          return;
+        }
+        // 入力された郵便番号を整形する
+        zipcode = zipcode.replace( "-", "" );
+
+        // 郵便番号を住所に変換してくれるAPIに問い合わせる
+        $http.get( 'https://api.zipaddress.net/' ,{params:{zipcode: zipcode}})
+        .success( function(data,status,headers,config ){
+          code = data.code;
+          if (code==200) {
+            $scope.result = data.data;
+            $scope.pref = data.data.pref;
+            $scope.address = data.data.address;
+            $scope.city = data.data.city;
+            $scope.town = data.data.town;
+            $scope.fullAddress = data.data.fullAddress;
+          }
+          else {
+            $scope.result = "住所不明";
+          }
+        })
+        .error( function(data,status,headers,config ){
+          $scope.result = "通信失敗";
+        });
+      };
+    }
+  ]
+);
+/**
+ * 全角から半角への変革関数
+ * 入力値の英数記号を半角変換して返却
+ * [引数]   strVal: 入力値
+ * [返却値] String(): 半角変換された文字列
+ *  (http://jquery.nj-clucker.com/change-double-byte-to-half-width/)
+ */
+function toHalfWidth(strVal){
+  // 半角変換
+  var halfVal = strVal.replace(/[！-～]/g,
+    function( tmpStr ) {
+      // 文字コードをシフト
+      return String.fromCharCode( tmpStr.charCodeAt(0) - 0xFEE0 );
+    }
+  );
+
+  // 文字コードシフトで対応できない文字の変換
+  return halfVal.replace(/”/g, "\"")
+    .replace(/’/g, "'")
+    .replace(/‘/g, "`")
+    .replace(/￥/g, "\\")
+    .replace(/　/g, " ")
+    .replace(/〜/g, "~");
+}
